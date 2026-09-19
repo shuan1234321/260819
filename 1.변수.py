6fcdc90db6fa659b75827f9638c9a94babf2f31a
User <srv.vinly@gmail.com>
2026-09-19 11:15:42 +0900

    경신고

---
+++ 1.변수.py
@@ -0,0 +1,20 @@
+a = 123
+print(a)
+
+a = "a"
+print(a)
+
+a = 123 + 123
+print(a)
+
+a = [1123, "sasd", 3, 5+3]
+print(a)
+
+var1 = 10
+var2 = 20
+
+a = var1 + var2
+print(a)
+
+input_var = input("숫자 하나 입력")
+print(input_var)
